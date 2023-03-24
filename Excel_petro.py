9743ddc8fa18a3ccc0e1cb3bfc72abef1b3cae7b
Gaston Herrlein <herrleingaston@gmail.com>
2023-03-24 03:13:23 -0300

    V1.0.2: Se crea una funcion que toma un df y devuelve otro df con el cuyo indice es el codigo y guarda la diferencia de las fechas consecutivas del mismo codigo

--- Excel_petro.py
+++ Excel_petro.py
@@ -4,7 +4,7 @@ import pandas as pd
 Control_Panol = "/home/gaston/Documentos/Gaston/Proyectos_Programacion/Excel_Petro/Datos/Control pañol P2.xls"; 
 Control_Llegada_Pedidos = "/home/gaston/Documentos/Gaston/Proyectos_Programacion/Excel_Petro/Datos/control llegada de pedidos.xlsx"; 
 
-
+#Funcion para leer el df de una planilla de excel
 def ReadData (root, sheet="Hoja1", fil=[4, 1034], col=[1, 11]):
     archivo_excel = pd.read_excel (root, sheet); 
     archivo_excel = archivo_excel.iloc [fil[0]:fil[1], col[0]:col[1]]; #PARA SESGAR LAS FILAS Y COLUMNAS DE INTERES
@@ -16,7 +16,7 @@ def ReadData (root, sheet="Hoja1", fil=[4, 1034], col=[1, 11]):
 
 DataBase = ReadData (Control_Panol, "ProductosStock"); 
 DataBase = DataBase.rename (columns = {"Dar click en GUARDAR cada vez que se carga un movimiento ": "Codigo", "Unnamed: 2": "Clase", "Unnamed: 3": "Categoria", "Unnamed: 4": "Fabricante","Unnamed: 5": "Nombre","Unnamed: 6": "Descripcion","Unnamed: 7": "Aclaracion","Unnamed: 8": "Valor inicial","Unnamed: 9": "Ingreso","Unnamed: 10": "Salida"}); 
-Database = DataBase.astype ({"Codigo": "int32", "Valor inicial": "float64", "Ingreso":"float64", "Salida":"float64"}); 
+DataBase = DataBase.astype ({"Codigo": "int32", "Valor inicial": "float64", "Ingreso":"float64", "Salida":"float64"}); 
 
 
 Salida = ReadData (Control_Panol, "Salidas", [4,513], [1,11]); 
@@ -37,37 +37,67 @@ Pedidos = Pedidos.rename (columns = {"Unnamed: 0": "Codigo", "Unnamed: 2": "Fech
 Pedidos = Pedidos.astype ({"Codigo": "int32", "Cantidad": "float64"}); 
 
 
-#print (DataBase); 
-#DataLong = DataBase.shape [1]; 
+#Funcion para generar un df con el nombre del codigo y la diferencia de dias en pedidos
+def average(df):
+    #Para recorrer las columnas
+    codigos = []; 
+    aux = [];  
+    fechas = []; 
+    cont=0; #variable para recorrer la lista fecha
 
-"""
-codigos = Salida ["Codigo"]; 
-print (codigos);   
-"""
+    df = df.sort_values (["Codigo","Fecha"]);  #Ordenamos la lista para trabajar
 
-#ESTE FOR SE PODRIA METER EN UNA FUNCION PARA REUTILIZARLO CON EL df DE ENTRADA 
-#Para recorrer las columnas
-codigos = []; 
-aux = [];  
-fechas = [];  
-
-Salida = Salida.sort_values ("Codigo");  #Ordenamos la lista para trabajar
-
-#Aprovechando que Pandas nos deja operar con datos de tipo DataTime operamos sobre las fechas para obtener la frecuencia de uso 
-# Para recorrer todo el dataframe -->"Salida.shape [0]
-for index in range (0,7):
-    if index!=0:
-        if Salida.iloc [index, 0]==Salida.iloc [(index-1), 0]:
-             aux.append (Salida.iloc [index, 1]-Salida.iloc [index-1, 1]); 
+    #Aprovechando que Pandas nos deja operar con datos de tipo DataTime operamos sobre las fechas para obtener la frecuencia de uso 
+    #Para recorrer todo el dataframe -->"df.shape [0]
+    for index in range (df.shape [0]): 
+        if index!=0:    #AGREGAR UN CONDICIONAL MAS PARA QUE FILTRE LOS 0 (ERRORES)
+            if df.iloc [index, 0]==df.iloc [(index-1), 0]:
+                aux.append (df.iloc [index, 1]-df.iloc [index-1, 1]); 
+            else:
+                codigos.append (df.iloc [index, 0]); 
+                fechas.append([]);  
+                for j in range (len(aux)): 
+                    fechas[cont].append(aux[j]); 
+                cont+=1;  
+                aux.clear(); 
         else:
-            codigos.append (Salida.iloc [index, 0]);  
-            print (aux); 
-            print ("\n"); 
-            fechas.append (aux); 
-            print (aux); 
-            #print (fechas); 
-            print ("\n \n"); 
-            aux.clear();  
-    else:
-        codigos.append (Salida.iloc [index, 0]); 
+            codigos.append (df.iloc [index, 0]); 
+    
+        if (index+1) == df.shape [0]: #Cuando llegue a la ultima iteracion
+            fechas.append([]); 
+            for j in range (len(aux)): 
+                fechas[cont+1].append(aux[j]); 
+            fechaSalida = pd.DataFrame(fechas, index= codigos); 
+    return fechaSalida; 
+     
 
+average (Salida); 
+
+
+
+#RESOLVI EL PROBLEMA APRA QUE NO ME BORRE LA VARIABLE FECHA
+
+
+
+#                   Dataframe Salida pre-ordenamiento
+# Index  
+#  |
+#     | Codigo |       Fecha         | Cantidad  <-- Columns
+# 4   |  801   | 2021-10-19 00:00:00 |   2.0
+# 5   |  2226  | 2021-10-20 00:00:00 |   3.0
+# 6   |  44    | 2021-10-20 00:00:00 |   4.0
+# 7   |  1914  | 2021-10-22 00:00:00 |   1.0
+# 8   |  799   | 2021-10-23 00:00:00 |   11.0
+
+
+
+#                   Dataframe Salida pos-ordenamiento
+# Index  
+#  |
+#  v
+#     | Codigo |       Fecha         | Cantidad  <-- Columns
+# 143 |   0    | 2022-02-02 00:00:00 |   0.0
+# 365 |   0    | 2022-08-16 00:00:00 |   0.0
+# 259 |  36    | 2022-05-16 00:00:00 |   2.0
+# 267 |  36    | 2022-05-25 00:00:00 |   10.0
+# 459 |  36    | 2022-10-03 00:00:00 |   2.0
