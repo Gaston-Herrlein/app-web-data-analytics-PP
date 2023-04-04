144ea0fa2c3bbcda99578f68ebc84df31adc6dd0
Gaston Herrlein <herrleingaston@gmail.com>
2023-04-04 01:27:00 -0300

    V1.1.3: Se refactoriza el codigo

--- Excel_petro.py
+++ Excel_petro.py
@@ -1,57 +1,22 @@
 import Libreria.ManejoDeDataFrame as mdf
 
-
 Control_Panol = "/home/gaston/Documentos/Gaston/Proyectos_Programacion/Excel_Petro/Datos/Control pañol P2.xls"; 
 Control_Llegada_Pedidos = "/home/gaston/Documentos/Gaston/Proyectos_Programacion/Excel_Petro/Datos/control llegada de pedidos.xlsx";  
 
-#fil = int (input ("Ingrese el numero de filas: ")); 
-#col = int (input ("ingrese el numero de columnas: ")); 
+def main ():
+    DataBase = mdf.ReadData (Control_Panol, "ProductosStock"); 
+    DataBase = mdf.Rename (DataBase,["Dar click en GUARDAR cada vez que se carga un movimiento ", "Unnamed: 2", "Unnamed: 3", "Unnamed: 4","Unnamed: 5","Unnamed: 6","Unnamed: 7", "Unnamed: 8", "Unnamed: 9", "Unnamed: 10"], ["Codigo", "Clase", "Categoria", "Fabricante", "Nombre", "Descripcion", "Aclaracion", "Valor inicial", "Ingreso", "Salida"], ["Codigo", "Valor inicial", "Ingreso", "Salida"], ["int32", "float64", "float64", "float64"]); 
 
-DataBase = mdf.ReadData (Control_Panol, "ProductosStock"); 
-DataBase = DataBase.rename (columns = {"Dar click en GUARDAR cada vez que se carga un movimiento ": "Codigo", "Unnamed: 2": "Clase", "Unnamed: 3": "Categoria", "Unnamed: 4": "Fabricante","Unnamed: 5": "Nombre","Unnamed: 6": "Descripcion","Unnamed: 7": "Aclaracion","Unnamed: 8": "Valor inicial","Unnamed: 9": "Ingreso","Unnamed: 10": "Salida"}); 
-DataBase = DataBase.astype ({"Codigo": "int32", "Valor inicial": "float64", "Ingreso":"float64", "Salida":"float64"}); 
+    Salida = mdf.ReadData (Control_Panol, "Salidas", [4,513], [1,11]); 
+    Salida = mdf.Rename (Salida, ["Dar click en GUARDAR cada vez que se carga un movimiento", "Unnamed: 8", "Unnamed: 9"], ["Codigo", "Fecha", "Cantidad"], ["Codigo", "Cantidad"], ["int32", "float64"], True, ["Unnamed: 2", "Unnamed: 3", "Unnamed: 4", "Unnamed: 5", "Unnamed: 6", "Unnamed: 7"]); 
 
+    Entrada = mdf.ReadData (Control_Panol, "Entradas",[4,822], [1,11]); 
+    Entrada = mdf.Rename (Entrada, ["Dar click en GUARDAR cada vez que se carga un movimiento", "Unnamed: 8", "Unnamed: 9"], ["Codigo", "Fecha", "Cantidad"], ["Codigo", "Cantidad"], ["int32", "float64"], True, ["Unnamed: 2", "Unnamed: 3", "Unnamed: 4", "Unnamed: 5", "Unnamed: 6", "Unnamed: 7"]);  
 
-Salida = mdf.ReadData (Control_Panol, "Salidas", [4,513], [1,11]); 
-Salida = Salida.drop (["Unnamed: 2", "Unnamed: 3", "Unnamed: 4", "Unnamed: 5", "Unnamed: 6", "Unnamed: 7"], axis=1); 
-Salida = Salida.rename (columns = {"Dar click en GUARDAR cada vez que se carga un movimiento": "Codigo", "Unnamed: 8": "Fecha", "Unnamed: 9": "Cantidad"}); 
-Salida = Salida.astype ({"Codigo": "int32", "Cantidad": "float64"}); 
+    Pedidos = mdf.ReadData (Control_Llegada_Pedidos, fil=[2,2399], col=[0,13]); 
+    Pedidos = mdf.Rename (Pedidos, ["Unnamed: 0", "Unnamed: 2", "Unnamed: 4", "Unnamed: 8", "Unnamed: 10", "Unnamed: 12"], ["Codigo", "Fecha Pedido", "Cantidad", "Estado", "¿Recibidos?", "Demora"], ["Codigo", "Cantidad"], ["int32", "float64"], True, ["seguimiento pedidos", "Unnamed: 3", "Unnamed: 5", "Unnamed: 6", "Aerosol Lubricante Deslizante Antiadherente Silox de Rolls x 220 gr  (Celeste)", "Unnamed: 9", "Unnamed: 11"]);  
 
+    #print (mdf.Average (Salida)); 
 
-Entrada = mdf.ReadData (Control_Panol, "Entradas",[4,822], [1,11]); 
-Entrada = Entrada.drop (["Unnamed: 2", "Unnamed: 3", "Unnamed: 4", "Unnamed: 5", "Unnamed: 6", "Unnamed: 7"], axis=1); 
-Entrada = Entrada.rename (columns = {"Dar click en GUARDAR cada vez que se carga un movimiento": "Codigo", "Unnamed: 8": "Fecha", "Unnamed: 9": "Cantidad"}); 
-Entrada = Entrada.astype ({"Codigo": "int32", "Cantidad": "float64"}); 
-
-
-Pedidos = mdf.ReadData (Control_Llegada_Pedidos, fil=[2,2399], col=[0,13]); 
-Pedidos = Pedidos.drop (["seguimiento pedidos", "Unnamed: 3", "Unnamed: 5", "Unnamed: 6", "Aerosol Lubricante Deslizante Antiadherente Silox de Rolls x 220 gr  (Celeste)", "Unnamed: 9", "Unnamed: 11"], axis=1); 
-Pedidos = Pedidos.rename (columns = {"Unnamed: 0": "Codigo", "Unnamed: 2": "Fecha Pedido", "Unnamed: 4": "Cantidad", "Unnamed: 8": "Estado", "Unnamed: 10": "¿Recibidos?", "Unnamed: 12": "Demora"}); 
-Pedidos = Pedidos.astype ({"Codigo": "int32", "Cantidad": "float64"}); 
-     
-
-print (mdf.average (Salida)); 
-
-
-#                   Dataframe Salida pre-ordenamiento
-# Index  
-#  |
-#     | Codigo |       Fecha         | Cantidad  <-- Columns
-# 4   |  801   | 2021-10-19 00:00:00 |   2.0
-# 5   |  2226  | 2021-10-20 00:00:00 |   3.0
-# 6   |  44    | 2021-10-20 00:00:00 |   4.0
-# 7   |  1914  | 2021-10-22 00:00:00 |   1.0
-# 8   |  799   | 2021-10-23 00:00:00 |   11.0
-
-
-
-#                   Dataframe Salida pos-ordenamiento
-# Index  
-#  |
-#  v
-#     | Codigo |       Fecha         | Cantidad  <-- Columns
-# 143 |   0    | 2022-02-02 00:00:00 |   0.0
-# 365 |   0    | 2022-08-16 00:00:00 |   0.0
-# 259 |  36    | 2022-05-16 00:00:00 |   2.0
-# 267 |  36    | 2022-05-25 00:00:00 |   10.0
-# 459 |  36    | 2022-08-23 00:00:00 |   2.0
+if __name__=='__main__':
+    main(); 
--- Libreria/ManejoDeDataFrame.py
+++ Libreria/ManejoDeDataFrame.py
@@ -8,36 +8,53 @@ def ReadData (root, sheet="Hoja1", fil=[4, 1034], col=[1, 11]):
     return archivo_excel; 
 
 
+#Funcion para arreglar el df leido
+#           df, [" "], [" "],  [" "], [""],   bool,            ["type"]  
+def Rename (df, name, rename, value, asTo, remove=False, indexRemove=[""]):
+    dataFrame=df;  
+    
+    for i in range (len(rename)): 
+        dataFrame = dataFrame.rename (columns = {name[i]: rename[i]}); 
+        
+    for i in range (len(value)):
+        dataFrame = dataFrame.astype ({value[i]: asTo[i]}); 
+    
+    if remove==True: 
+        dataFrame = dataFrame.drop (indexRemove, axis=1); 
+    
+    return dataFrame; 
+
+
 #Funcion para generar un df con el nombre del codigo y la diferencia de dias en pedidos
-def average(df):
+def Average(df):
+    dataFrame = df; 
     #Para recorrer las columnas
     codigos = []; 
     aux = [];  
     fechas = []; 
     cont=0; #variable para recorrer la lista fecha
+    dataFrame = dataFrame.sort_values (["Codigo","Fecha"]);  #Ordenamos la lista para trabajar
 
-    df = df.sort_values (["Codigo","Fecha"]);  #Ordenamos la lista para trabajar
-
-    #Operamos sobre las fechas con el tipo de dato DataTime 
     #Para recorrer todo el dataframe --> "df.shape [0]"
-    for index in range (df.shape [0]): 
+    for index in range (dataFrame.shape [0]): 
         if index!=0:    #AGREGAR UN CONDICIONAL MAS PARA QUE FILTRE LOS 0 (ERRORES)
-            if df.iloc [index, 0]==df.iloc [(index-1), 0]:
-                aux.append (df.iloc [index, 1]-df.iloc [index-1, 1]); 
+            if dataFrame.iloc [index, 0]==dataFrame.iloc [(index-1), 0]:
+                aux.append (dataFrame.iloc [index, 1]-dataFrame.iloc [index-1, 1]); 
             else:
-                codigos.append (df.iloc [index, 0]); 
+                codigos.append (dataFrame.iloc [index, 0]); 
                 fechas.append([]);  
                 for j in range (len(aux)): 
                     fechas[cont].append(aux[j]); 
                 cont+=1;  
                 aux.clear(); 
         else:
-            codigos.append (df.iloc [index, 0]); 
+            codigos.append (dataFrame.iloc [index, 0]); 
     
-        if (index+1) == df.shape [0]: #Cuando llegue a la ultima iteracion
+        if (index+1) == dataFrame.shape [0]: #Cuando llegue a la ultima iteracion
             fechas.append([]); 
             for j in range (len(aux)): 
                 fechas[cont+1].append(aux[j]); 
             fechaSalida = pd.DataFrame(fechas, index= codigos); 
     return fechaSalida; 
+
 
